282e4014c1064e596ef0ed0da4f98fe06c1ec1b7
Fran Hoey <fran@bluejumper.com>
2019-08-08 07:14:23 +0100

    Added .Net Core Asp.net Project template

---
+++ ChesterDevs.Core.Aspnet/ChesterDevs.Core.Aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChesterDevs.Core.Aspnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
