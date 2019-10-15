05209a42e3a254085b6a5b0f1e35c571dc84e55c
Fran Hoey <fran@bluejumper.com>
2019-10-15 07:09:00 +0100

    Added content to Contributors page

---
+++ ChesterDevs.Core.Aspnet/ChesterDevs.Core.Aspnet/wwwroot/js/Pages/contributors.js
@@ -0,0 +1,11 @@
+﻿window.onload = function loadContributors() {
+    fetch("https://api.github.com/repos/ChesterDevs/chester-devs-website/contributors")
+        .then((response) => { if (response.ok) { return response.json() } })
+        .then((contributors) => {
+            let list = "";
+            contributors.forEach((contributor, index) => {
+                list += `<div style="clear:both; margin: 8px; height: 80px; width: 600px;"><img src="${contributor.avatar_url}" alt="${contributor.login}" style="float:left; width:80px;"><div style="float:left; margin-left: 12px;"><h3>#${index + 1} (<a href="https://github.com/ChesterDevs/chester-devs-website/commits/master?author=${contributor.login}">${contributor.contributions} commits</a>)</h3><h2>${contributor.login}</h2><a href="${contributor.url}">${contributor.url}</a></div></div>`
+            })
+            document.getElementById("contributer-list").innerHTML = list;
+        })
+};
