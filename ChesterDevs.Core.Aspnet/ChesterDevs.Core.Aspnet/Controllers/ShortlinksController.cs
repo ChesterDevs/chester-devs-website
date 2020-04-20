c49914a0aadf499cfe46aa21d4cac415bf60f129
Fran Hoey <fran@bluejumper.com>
2020-04-20 19:38:27 +0100

    Added Feedback redirect

---
+++ ChesterDevs.Core.Aspnet/ChesterDevs.Core.Aspnet/Controllers/ShortlinksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChesterDevs.Core.Aspnet.Controllers
+{
+    public class ShortlinksController : Controller
+    {
+        [Route("/Feedback")]
+
+        public IActionResult Feedback()
+        {
+            return RedirectPermanent("https://docs.google.com/forms/d/e/1FAIpQLSdcayU15CTT5mtR3141XavCY30bBoSBwmlZBbeif4hwpXkgUQ/viewform");
+        }
+    }
+}
