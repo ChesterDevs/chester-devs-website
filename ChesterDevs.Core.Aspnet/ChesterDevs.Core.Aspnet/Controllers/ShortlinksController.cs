71aa6253b8aa20aa0233bbf4597cf3be047bd5ac
Fran Hoey <fran@bluejumper.com>
2020-07-10 10:46:45 +0100

    Added Slack short link to the a Slack generated invite url

--- ChesterDevs.Core.Aspnet/ChesterDevs.Core.Aspnet/Controllers/ShortlinksController.cs
+++ ChesterDevs.Core.Aspnet/ChesterDevs.Core.Aspnet/Controllers/ShortlinksController.cs
@@ -9,10 +9,15 @@ namespace ChesterDevs.Core.Aspnet.Controllers
     public class ShortlinksController : Controller
     {
         [Route("/Feedback")]
-
         public IActionResult Feedback()
         {
             return RedirectPermanent("https://fran682371.typeform.com/to/sISmVB");
+        }
+
+        [Route("/Slack")]
+        public IActionResult Slack()
+        {
+            return RedirectPermanent("https://join.slack.com/t/chesterdevs/shared_invite/zt-fk2t22wn-TNZ_tqp4JKE~lnKYthqThQ");
         }
     }
 }
